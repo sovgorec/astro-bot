4bd81793fbd1cdedb9ccc40cfe984f48356e932e
sovgorec <sovgorec@gmail.com>
2025-11-05 11:17:41 -0300

    Create zodiac.ts

---
+++ src/zodiac.ts
@@ -0,0 +1,14 @@
+export const zodiacList = [
+  { key: "aries", name: "Овен", emoji: "♈" },
+  { key: "taurus", name: "Телец", emoji: "♉" },
+  { key: "gemini", name: "Близнецы", emoji: "♊" },
+  { key: "cancer", name: "Рак", emoji: "♋" },
+  { key: "leo", name: "Лев", emoji: "♌" },
+  { key: "virgo", name: "Дева", emoji: "♍" },
+  { key: "libra", name: "Весы", emoji: "♎" },
+  { key: "scorpio", name: "Скорпион", emoji: "♏" },
+  { key: "sagittarius", name: "Стрелец", emoji: "♐" },
+  { key: "capricorn", name: "Козерог", emoji: "♑" },
+  { key: "aquarius", name: "Водолей", emoji: "♒" },
+  { key: "pisces", name: "Рыбы", emoji: "♓" }
+];
