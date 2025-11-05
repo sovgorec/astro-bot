39d60613df8457f59c88e03ffae2135b46c93df6
sovgorec <sovgorec@gmail.com>
2025-11-05 11:16:56 -0300

    Create bot.ts

---
+++ src/bot.ts
@@ -0,0 +1,26 @@
+import { Telegraf, Markup } from "telegraf";
+import dotenv from "dotenv";
+import { zodiacList } from "./zodiac.js";
+
+dotenv.config();
+const bot = new Telegraf(process.env.BOT_TOKEN!);
+
+bot.start((ctx) => {
+  const buttons = zodiacList.map((z) => [
+    Markup.button.callback(`${z.emoji} ${z.name}`, `zodiac_${z.key}`)
+  ]);
+
+  ctx.reply(
+    "🌟 Привет! Я астробот.\nВыбери свой знак Зодиака:",
+    Markup.inlineKeyboard(buttons)
+  );
+});
+
+bot.action(/zodiac_(.+)/, async (ctx) => {
+  const sign = ctx.match[1];
+  await ctx.answerCbQuery();
+  await ctx.reply(`Ваш знак — ${sign} 🔮`);
+});
+
+bot.launch();
+console.log("✅ Astro-bot запущен!");
